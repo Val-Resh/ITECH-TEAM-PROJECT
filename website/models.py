72c6fab9e4b468e289756179b95031c235a3b5ec
Val Resh <reshetnyakvalle@gmail.com>
2022-02-20 17:26:43 +0000

    added the User and Monster models with unit tests.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.core.validators import *
 
 # Create your models here.
+
+class Monster(models.Model):
+    MAX_NAME_LENGTH = 30
+    MAX_LEVEL = 5
+    MAX_HP = 500
+    MAX_AP = 100
+    MAX_EXP = 1000
+    STARTING_HEALTH = MAX_HP / MAX_LEVEL
+    STARTING_LEVEL = MAX_LEVEL / MAX_LEVEL
+    STARTING_ATTACK = MAX_AP / MAX_LEVEL
+
+    name = models.CharField(max_length = MAX_NAME_LENGTH)
+    level = models.IntegerField(default=STARTING_LEVEL, validators=[MaxValueValidator(MAX_LEVEL),
+                                           MinValueValidator(STARTING_LEVEL)])
+    health = models.IntegerField(default=STARTING_HEALTH, validators=[MaxValueValidator(MAX_HP),
+                                           MinValueValidator(0)])
+    attack = models.IntegerField(default = STARTING_ATTACK, validators=[MaxValueValidator(MAX_AP),
+                                           MinValueValidator(STARTING_ATTACK)])
+    exp = models.IntegerField(default = 0, validators=[MaxValueValidator(MAX_EXP),
+                                           MinValueValidator(0)])
+
+    #method to level up a monster.
+    def level_up(self):
+        if self.level < self.MAX_LEVEL and self.exp == self.MAX_EXP:
+            self.level += 1
+            self.health += 100
+            self.attack += 20
+            self.exp = 0  
+
+
+class User(models.Model):
+    MAX_COINS = 1000
+    MAX_PASSWORD_LENGTH = 30
+    MAX_USERNAME_LENGTH = 30
+    MAX_WINS = 15
+
+    username = models.CharField(max_length = MAX_USERNAME_LENGTH)
+    password = models.CharField(max_length = MAX_PASSWORD_LENGTH)
+    wins = models.IntegerField(default = 0, validators=[MaxValueValidator(MAX_WINS),
+                                           MinValueValidator(0)])
+    monster = models.OneToOneField(Monster, on_delete=models.SET_NULL, default = None, null=True)
+    coins = models.IntegerField(default = 0, validators=[MaxValueValidator(MAX_COINS),
+                                           MinValueValidator(0)])
+
+    #method to add coins to a User.
+    def add_coins(self, coin_amount):
+        if coin_amount > 0:
+            self.coins = self.MAX_COINS if coin_amount > self.MAX_COINS \
+                            else self.coins + coin_amount
--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from website.models import *
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    def test_attributes(self):
+        user = User(username="username", password="password")
+        self.assertEqual(user.username, "username")
+        self.assertEqual(user.password, "password")
+
+    def test_add_coins(self):
+        user = User(username="username", password="password")
+        self.assertEqual(user.coins, 0)
+        user.add_coins(100)
+        self.assertEqual(user.coins, 100)
+        user.add_coins(10000000)
+        self.assertEqual(user.coins, user.MAX_COINS)
+        user.coins = 0
+        user.add_coins(-1000)
+        self.assertEqual(user.coins, 0)
+
+class MonsterTest(TestCase):
+    
+    def test_default_values(self):
+        monster = Monster(name="monster")
+        expected = ["monster", Monster.STARTING_LEVEL, Monster.STARTING_HEALTH, Monster.STARTING_ATTACK, 0]
+        testing = [monster.name, monster.level, monster.health, monster.attack, monster.exp]
+        for i in range(len(expected)):
+            self.assertEqual(testing[i], expected[i])
+    
+    def test_level_up(self):
+        monster = Monster(name="monster")
+        monster.exp = Monster.MAX_EXP
+        monster.level_up()
+        expected = ["monster", Monster.STARTING_LEVEL+1, Monster.STARTING_HEALTH+100, Monster.STARTING_ATTACK+20, 0]
+        testing = [monster.name, monster.level, monster.health, monster.attack, monster.exp]
+        for i in range(len(expected)):
+            self.assertEqual(testing[i], expected[i])
+
+class GiveUserMonster(TestCase):
+
+    def test_give_monster(self):
+        user = User(username="username", password="password")
+        monster = Monster(name="monster")
+        monsterTwo = Monster(name="monsterTwo")
+        user.monster = monster
+        self.assertTrue(user.monster == monster)
