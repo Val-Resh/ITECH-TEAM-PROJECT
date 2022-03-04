6690464c6b1f1421819b33f301c9758dac9947c3
Penpicha Sinrattanavong <--local>
2022-03-04 18:17:01 +0000

    first init views

--- website/views.py
+++ website/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def elements(request):
+    return render(request, 'elements.html')
+
+
+def userProfile(request):
+    return render(request, 'user-profile.html')
