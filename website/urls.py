6e62699bc2b6e709a01a87dc2b38722264e5b0f2
Penpicha Sinrattanavong <--local>
2022-03-05 11:28:24 +0000

    comment thing didn't use

--- website/urls.py
+++ website/urls.py
@@ -4,10 +4,10 @@ from website import views
 
 app_name = 'studymonster'
 
-urlpatterns = [
-    path('', views.index, name='index'),  
-    path('login/', views.login, name='login'),
-    path('shop/', views.shop, name='shop'),
-    path('userprofile/', views.userprofile, name='user profile'),
-    path('room/', views.room, name='room'),
-]
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('login/', views.login, name='login'),
+#     path('shop/', views.shop, name='shop'),
+#     path('userprofile/', views.userprofile, name='user profile'),
+#     path('room/', views.room, name='room'),
+# ]
