1461995dfe15cd9561c9400c9a0602cc66354213
Penpicha Sinrattanavong <--local>
2022-03-06 11:48:03 +0000

    add monster page

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from website.models import User, Room, Item
+from website.models import User, Room, Item, Monster
 
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Item)
+admin.site.register(Monster)
